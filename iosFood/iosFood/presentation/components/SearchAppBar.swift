b6188b650f7514e846447177d1a666394d4c9276
Vladimir.Tambovtsev <tambovcev99@gmail.com>
2022-02-25 14:06:45 +0300

    make horizontal scroll for cateogries pills

--- iosFood/iosFood/presentation/components/SearchAppBar.swift
+++ iosFood/iosFood/presentation/components/SearchAppBar.swift
@@ -11,38 +11,55 @@ import SwiftUI
 import shared
 
 struct SearchAppBar: View {
-
+    
     @State var query: String
-    let onTriggerEvent: (RecipeListEvents) -> Void
-
+    private let foodCategories: [FoodCategory]
+    private let onTriggerEvent: (RecipeListEvents) -> Void
+    
     init(
-            query: String,
-            onTriggerEvent: @escaping (RecipeListEvents) -> Void
+        query: String,
+        foodCategories: [FoodCategory],
+        onTriggerEvent: @escaping (RecipeListEvents) -> Void
     ) {
+        self.foodCategories = foodCategories
         self.onTriggerEvent = onTriggerEvent
         self._query = State(initialValue: query) // set initial value for query
     }
-
+    
     var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "magnifyingglass")
-                TextField(
+            VStack{
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                    TextField(
                         "Search...",
                         text: $query,
                         onCommit:{
                             onTriggerEvent(RecipeListEvents.NewSearch())
                         }
-                )
-                        .onChange(of: query, perform: { value in
-                            onTriggerEvent(RecipeListEvents.OnUpdateQuery(query: value))
-                        })
+                    )
+                    .onChange(of: query, perform: { value in
+                        onTriggerEvent(RecipeListEvents.OnUpdateQuery(query: value))
+                    })
+                }
+                .padding(.bottom, 8)
+                ScrollView(.horizontal){
+                    HStack(spacing: 10){
+                        ForEach(foodCategories, id: \.self){ category in
+                            FoodCategoryChip(
+                                category: category.value,
+                                isSelected: false // TODO
+                            )
+                            .onTapGesture {
+                                query = category.value
+                                // TODO
+                            }
+                        }
+                    }
+                }
             }
-                    .padding(.bottom, 8)
+            .padding(.top, 8)
+            .padding(.leading, 8)
+            .padding(.trailing, 8)
+            .background(Color.white.opacity(0))
         }
-                .padding(.top, 8)
-                .padding(.leading, 8)
-                .padding(.trailing, 8)
-                .background(Color.white.opacity(0))
-    }
 }
--- iosFood/iosFood/presentation/recipe_list/RecipeListScreen.swift
+++ iosFood/iosFood/presentation/recipe_list/RecipeListScreen.swift
@@ -10,57 +10,59 @@ import SwiftUI
 import shared
 
 struct RecipeListScreen: View {
-
+    
     // dependencies
     private let networkModule: NetworkModule
     private let cacheModule: CacheModule
     private let searchRecipesModule: SearchRecipesModule
-
+    private let foodCategories: [FoodCategory]
+    
     @ObservedObject var viewModel: RecipeListViewModel
-
+    
     init(
-            networkModule: NetworkModule,
-            cacheModule: CacheModule
+        networkModule: NetworkModule,
+        cacheModule: CacheModule
     ) {
         self.networkModule = networkModule
         self.cacheModule = cacheModule
         self.searchRecipesModule = SearchRecipesModule(
-                networkModule: self.networkModule,
-                cacheModule: self.cacheModule
+            networkModule: self.networkModule,
+            cacheModule: self.cacheModule
         )
+        let foodCategoryUtil = FoodCategoryUtil()
         self.viewModel = RecipeListViewModel(
-                searchRecipes: searchRecipesModule.searchRecipes,
-                foodCategoryUtil: FoodCategoryUtil()
+            searchRecipes: searchRecipesModule.searchRecipes,
+            foodCategoryUtil: foodCategoryUtil
         )
+        self.foodCategories = foodCategoryUtil.getAllFoodCategories()
     }
-
+    
     var body: some View {
         VStack{
             HStack{
                 Text("Page: \(viewModel.state.page), Size: \(viewModel.state.recipes.count)")
-                        .padding()
+                    .padding()
             }
             SearchAppBar(
-                    query: viewModel.state.query,
-                    onTriggerEvent: { event in
-                        viewModel.onTriggerEvent(stateEvent: event)
-                    }
+                query: viewModel.state.query,
+                foodCategories: foodCategories,
+                onTriggerEvent: { event in
+                    viewModel.onTriggerEvent(stateEvent: event)
+                }
             )
             List{
                 ForEach(viewModel.state.recipes, id: \.self.id){ recipe in
                     Text(recipe.title)
-                            .onAppear(perform: {
-                                if viewModel.shouldQueryNextPage(recipe: recipe){
-                                    viewModel.onTriggerEvent(stateEvent: RecipeListEvents.NextPage())
-                                }
-                            })
+                        .onAppear(perform: {
+                           if viewModel.shouldQueryNextPage(recipe: recipe){
+                               viewModel.onTriggerEvent(stateEvent: RecipeListEvents.NextPage())
+                           }
+                       })
                 }
             }
         }
     }
 }
-
-
 
 
 //struct RecipeListScreen_Previews: PreviewProvider {
